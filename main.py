c8439fda8158a6e71f7bd1f7813793d27aa0a448
junliangliu <j568liu@gmail.com>
2025-01-17 12:55:26 -0800

    hello yusuke

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-from matplotlib.ticker import FuncFormatter
+from matplotlib.ticker import FuncFormatter, ScalarFormatter
 
 # Load data
 data = pd.read_csv('YS-DI-013.csv')
@@ -15,15 +15,22 @@ def format_two_decimals(x, pos):
     return f'{x:.2f}'
 
 
+def format_y_axis(x, pos):
+    return f'{x / 1e6:.2f}'
+
+
 plt.figure(figsize=(10, 6))
 plt.xlabel('Relative Time (hours)')
 for column in data.select_dtypes(include=['float64', 'int64']).columns:
     if column != 'RelativeTime_Hours':
         plt.plot(data['RelativeTime_Hours'], data[column], label=column)
-plt.ylabel("Peak Area")
+plt.ylabel('Peak Area ($\\times 10^6$)')
+
 # Format axes to show 2 decimal places
 plt.gca().xaxis.set_major_formatter(FuncFormatter(format_two_decimals))
-plt.gca().yaxis.set_major_formatter(FuncFormatter(format_two_decimals))
+# plt.gca().yaxis.set_major_formatter(FuncFormatter(format_two_decimals))
+plt.gca().yaxis.set_major_formatter(FuncFormatter(format_y_axis))
+
 
 plt.title('Peak Area vs Relative Time (hours)')
 plt.legend(loc='upper center', bbox_to_anchor=(0.5, 1.15), ncol=3)  # Adjust ncol for the number of columns
